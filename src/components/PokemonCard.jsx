52cc32c315ec3e80b5ec3e8391a5b8b206179fb9
Vincent DESBROSSES <vincedsb@gmail.com>
2023-03-30 12:23:07 +0200

    React Basics 06 - State

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -28,7 +28,7 @@ function PokemonCard({pokemon}) {
 PokemonCard.propTypes = {
     pokemon: PropTypes.shape({
       name: PropTypes.string.isRequired,
-      imgSrc: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
     })
   }
 
